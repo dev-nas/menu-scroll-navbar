c2b2cb4577270620fd4ea488cdc1d0788c4fb117
dev-nas <50103693+dev-nas@users.noreply.github.com>
2019-08-01 12:29:35 +0200

    Add files via upload

---
+++ exercice 3 javascript le 1:08:2019/main.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector('#navbar');
+
+
+/*querySelector c'est pour sélectionner la navbar*/
+/*window c pour clicker sur toute la page*/
+/*scroll remplace le click,
+après le 150Px commence le scroll*/
+
+window.addEventListener('scroll', function() {
+      if(si mon scoll est superieur à 150 px alors faire){
+      	//ajouter une class CSS permettant de fixer ma navbar
+      };
+    });
+
+/*Il faut que je trouve comment déclencher un évènement avec une const
+et fixer la navbar avec js/*
